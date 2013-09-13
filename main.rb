1c73e9aaf9307697aec93910eb0a2cd7508ea57f
rcrespo9 <rudycrespo@hushmail.com>
2013-09-13 10:37:59 -0400

    Some edits

--- main.rb
+++ main.rb
@@ -1,17 +1,4 @@
 require 'sinatra'
-require 'sinatra/reloader'
-# require 'active_support/all'
-# require 'active_record'
-
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "postgresql",
-#   :host => "localhost",
-#   :username => "postgres",
-#   :password => "password",
-#   :database => "dogs_and_toys"
-# )
-
-# ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 get '/' do
 	erb :home
